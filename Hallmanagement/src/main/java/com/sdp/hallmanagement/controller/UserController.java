eb4f15a4d7c261bb542fdd1d5c8bbeb2de9f7be5
Vishmitha-Kumar <vishmithak05@gmail.com>
2024-08-02 12:03:50 +0530

    check

--- Hallmanagement/src/main/java/com/sdp/hallmanagement/controller/UserController.java
+++ Hallmanagement/src/main/java/com/sdp/hallmanagement/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sdp.hallmanagement.module.User;
 import com.sdp.hallmanagement.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
