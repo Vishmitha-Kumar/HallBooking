7c6d6ef05fedbef9ba3cc25665a7080f890555ec
Vishmitha-Kumar <vishmithak05@gmail.com>
2024-08-04 16:48:24 +0530

    review

--- Hallmanagement/src/main/java/com/sdp/hallmanagement/controller/ReviewController.java
+++ Hallmanagement/src/main/java/com/sdp/hallmanagement/controller/ReviewController.java
@@ -1,10 +1,21 @@
 package com.sdp.hallmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sdp.hallmanagement.module.Review;
 import com.sdp.hallmanagement.service.ReviewService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("/api/review")
@@ -13,6 +24,29 @@ public class ReviewController {
     @Autowired
     private ReviewService rs;
 
+    @PostMapping("/postReview")
+    public Review postReview(@RequestBody Review review ){
+        return rs.postReview(review);
+    }
+
+
+    @PutMapping("updateReview/{id}")
+    public Review updateReview(@PathVariable Long id, @RequestBody Review review) {
+        return rs.updateReview(id,review);
+       }
+ 
     
+    @DeleteMapping("deleteReview/{id}")
+    public String deleteReview(@PathVariable Long id){
+        return rs.deleteReview(id);
+       }
+      @GetMapping("/getReview/{id}")
+   public ResponseEntity<?> getReviews(@PathVariable Long id){
+    return new ResponseEntity<>(rs.getReviews(id),HttpStatus.OK);
+   }
     
+   @GetMapping("/getReviews")
+   public ResponseEntity<?> getAllReview(){
+    return new ResponseEntity<>(rs.getAllReview(),HttpStatus.OK);
+   }
 }
--- Hallmanagement/src/main/java/com/sdp/hallmanagement/repository/ReviewRepository.java
+++ Hallmanagement/src/main/java/com/sdp/hallmanagement/repository/ReviewRepository.java
@@ -1,5 +1,13 @@
 package com.sdp.hallmanagement.repository;
 
-public class ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sdp.hallmanagement.module.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review ,Long>{
+
+    
     
 }
